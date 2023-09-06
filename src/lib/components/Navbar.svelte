6fa730a1909685e743777da765a8a65c2a07bcb7
Damas Amirul Karim <damas@majoo.id>
2023-09-07 00:50:05 +0700

    add link to navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,7 +5,7 @@
 <nav class="sticky top-0 w-full bg-black text-white py-6 z-10">
 	<div class="flex justify-between container mx-auto">
 		<div class="flex items-center space-x-16">
-			<img src="/images/logo.svg" alt="Klik Resi Logo" width={99} height={13} />
+			<a href="/"><img src="/images/logo.svg" alt="Klik Resi Logo" width={99} height={13} /></a>
 			<div class="flex space-x-10">
 				<a href="/#beranda">Beranda</a>
 				<a href="/#produk">Produk</a>
