6e2a72055a97b40a2e639812e76491d9d06e5674
Damas Amirul Karim <damas@majoo.id>
2023-09-08 11:27:51 +0700

    change hero responsive

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -43,25 +43,25 @@
 	]
 </script>
 
-<section class="py-28 text-white bg-black scroll-mt-28" id="beranda">
-	<div class="container mx-auto">
-		<div class="flex items-center space-x-24">
-			<div class="flex flex-col w-1/2">
+<section class="py-10 md:py-28 text-white bg-black scroll-mt-28" id="beranda">
+	<div class="container mx-auto text-center md:text-left">
+		<div class="flex md:items-center flex-col md:flex-row space-y-16 md:space-y-0 md:space-x-24">
+			<div class="flex flex-col md:w-1/2">
 				<div class="flex flex-col space-y-4">
-					<h1 class="text-4xl font-bold font-heading">Paket Kamu Sudah Sampai Mana?</h1>
+					<h1 class="text-3xl md:text-4xl font-bold font-heading">Paket Kamu Sudah Sampai Mana?</h1>
 					<p>Tidak perlu bingung, kamu bisa memonitor semua pengirimanmu secara real-time.</p>
 				</div>
 				<div class="mt-12">
 					<a class="outline px-6 py-3 rounded-lg" href="#produk">Pelajari Lebih Lanjut</a>
 				</div>
 			</div>
-			<div class="relative w-1/2">
+			<div class="relative md:w-1/2">
 				<img src="/images/dashboard.webp" class="w-full h-full rounded-lg" alt="Hero" />
 			</div>
 		</div>
-		<div class="mt-32">
+		<div class="mt-16 md:mt-32">
 			<h2 class="font font-semibold mb-4 font-heading">Kami mendukung lebih dari 10+ ekspedisi terbaik di indonesia</h2>
-			<div class="flex justify-start space-x-2">
+			<div class="flex justify-center md:justify-start space-x-2">
 				{#each couriers as courier}
 					<img
 						src={`/icons/${courier.icon}.svg`}
